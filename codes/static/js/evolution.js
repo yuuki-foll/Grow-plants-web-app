a4d34e3abf6a15fedbf9ac86a03e7dbf8505560b
yuuki-foll <tu.over.ym@gmail.com>
2021-09-08 23:57:33 +0900

    add evolution animation

---
+++ codes/static/js/evolution.js
@@ -0,0 +1,23 @@
+/* 画像の進化アニメーション（点滅）を動かす */
+// 進化前→進化後→進化前と累計5回画像を切り替える
+function plant_evolution(img, evo_num) {
+    slide_img = new Array(img[evo_num -1],img[evo_num]);
+    count = -1;
+    allCount = 0;
+    imgTimer();
+}
+
+/* 画像を切り替える関数 */
+function imgTimer() {
+    //画像番号
+    count++; //*3
+    allCount++;
+    //画像の枚数確認
+    if (count == slide_img.length) count = 0; 
+    //画像出力
+    plant_pic.src = slide_img[count];
+    //次のタイマー呼びだし
+    if (allCount <= 5) {
+        setTimeout("imgTimer()", 200);
+    }
+}
